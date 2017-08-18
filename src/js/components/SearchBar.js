00fa1607960296c4ecc8a260cdd672ada5d4d8a2
grini93 <grini93@gmail.com>
2017-08-18 14:40:32 +0200

    search bar

--- src/js/components/SearchBar.js
+++ src/js/components/SearchBar.js
@@ -4,7 +4,12 @@ export default class SearchBar extends Component {
     render(){
         return (
             <div className="search-bar">
-
+                <form action="" className="search-bar__form">
+                    <input className="search-bar__input" type="text"/>
+                    <button className="search-bar__button">
+                        <i className="fa fa-search"> </i>
+                    </button>
+                </form>
             </div>
         )
     }
